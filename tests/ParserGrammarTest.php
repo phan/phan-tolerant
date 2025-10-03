0c546e4b0a516c9a87fb9ed65787b4357c7de2db
Daniel Leech <daniel@dantleech.com>
2025-10-03 09:11:48 +0100

    Tests: Improve test DX
    - Generate snapshot if file does not exist rather than whatever it was
      doing before.
    - Upgrade phpunit
    - Removed CallbackTestListener
    - Add hint about regeneration in assertions

--- tests/ParserGrammarTest.php
+++ tests/ParserGrammarTest.php
@@ -12,66 +12,43 @@ use PHPUnit\Framework\TestListener;
 use PHPUnit\Framework\TestListenerDefaultImplementation;
 use PHPUnit\Framework\AssertionFailedError;
 
-require_once __DIR__ . '/CallbackTestListener.php';
-
 class ParserGrammarTest extends TestCase {
-    private $expectedTokensFile;
-    private $expectedDiagnosticsFile;
-    private $tokens;
-    private $diagnostics;
-    public function run(?TestResult $result = null) : TestResult {
-        if (!isset($GLOBALS["GIT_CHECKOUT_PARSER"])) {
-            $GLOBALS["GIT_CHECKOUT_PARSER"] = true;
-            exec("git -C " . dirname(self::FILE_PATTERN) . " checkout *.php.tree *.php.diag");
-        }
-
-        $result->addListener(new CallbackTestListener(function (Test $test) {
-            if (isset($test->expectedTokensFile) && isset($test->tokens)) {
-                file_put_contents($test->expectedTokensFile, str_replace("\r\n", "\n", $test->tokens));
-            }
-            if (isset($test->expectedDiagnosticsFile) && isset($test->diagnostics)) {
-                file_put_contents($test->expectedDiagnosticsFile, str_replace("\r\n", "\n", $test->diagnostics));
-            }
-        }));
-
-        $result = parent::run($result);
-        return $result;
-    }
-
     /**
      * @dataProvider treeProvider
      */
     public function testOutputTreeClassificationAndLength($testCaseFile, $expectedTokensFile, $expectedDiagnosticsFile) {
-        $this->expectedTokensFile = $expectedTokensFile;
-        $this->expectedDiagnosticsFile = $expectedDiagnosticsFile;
-
         $fileContents = file_get_contents($testCaseFile);
-        if (!file_exists($expectedTokensFile)) {
-            file_put_contents($expectedTokensFile, $fileContents);
-            exec("git add " . $expectedTokensFile);
-        }
-
-        if (!file_exists($expectedDiagnosticsFile)) {
-            file_put_contents($expectedDiagnosticsFile, $fileContents);
-            exec("git add " . $expectedDiagnosticsFile);
-        }
 
         $parser = new \Microsoft\PhpParser\Parser();
         $sourceFileNode = $parser->parseSourceFile($fileContents);
-
-        $expectedTokens = str_replace("\r\n", "\n", file_get_contents($expectedTokensFile));
-        $expectedDiagnostics = str_replace("\r\n", "\n", file_get_contents($expectedDiagnosticsFile));
 
         $GLOBALS["SHORT_TOKEN_SERIALIZE"] = true;
         $tokens = str_replace("\r\n", "\n", json_encode($sourceFileNode, JSON_PRETTY_PRINT));
         $diagnostics = str_replace("\r\n", "\n", json_encode(\Microsoft\PhpParser\DiagnosticsProvider::getDiagnostics($sourceFileNode), JSON_PRETTY_PRINT));
         $GLOBALS["SHORT_TOKEN_SERIALIZE"] = false;
 
-        $this->tokens = $tokens;
-        $this->diagnostics = $diagnostics;
+        $skip = false;
+        if (!file_exists($expectedTokensFile)) {
+            file_put_contents($expectedTokensFile, $tokens);
+            $skip = true;
+        } else {
+            $expectedTokens = trim(str_replace("\r\n", "\n", file_get_contents($expectedTokensFile)));
+        }
 
-        $tokensOutputStr = "input doc:\r\n$fileContents\r\n\r\ninput: $testCaseFile\r\nexpected: $expectedTokensFile";
-        $diagnosticsOutputStr = "input doc:\r\n$fileContents\r\n\r\ninput: $testCaseFile\r\nexpected: $expectedDiagnosticsFile";
+
+        if (!file_exists($expectedDiagnosticsFile)) {
+            file_put_contents($expectedDiagnosticsFile, $diagnostics);
+            $skip = true;
+        } else {
+            $expectedDiagnostics = trim(str_replace("\r\n", "\n", file_get_contents($expectedDiagnosticsFile)));
+        }
+
+        if ($skip) {
+            self::markTestSkipped('Snapshot generated');
+        }
+
+        $tokensOutputStr = "input doc:\r\n$fileContents\r\n\r\ninput: $testCaseFile\r\nexpected: $expectedTokensFile (deleted expected file to regenerate)";
+        $diagnosticsOutputStr = "input doc:\r\n$fileContents\r\n\r\ninput: $testCaseFile\r\nexpected: $expectedDiagnosticsFile (delete expected file to regenerate)";
 
         $this->assertEquals($expectedTokens, $tokens, $tokensOutputStr);
         $this->assertEquals($expectedDiagnostics, $diagnostics, $diagnosticsOutputStr);
@@ -104,12 +81,14 @@ class ParserGrammarTest extends TestCase {
             $testProviderArray[basename($testCase)] = [$testCase, $testCase . ".tree", $testCase . ".diag"];
         }
 
-        foreach (self::PATTERNS_FOR_MINIMUM_PHP_VERSION as list($minVersionId, $filePattern)) {
-            if (PHP_VERSION_ID >= $minVersionId) {
-                $testCases = glob($filePattern . ".php");
-                foreach ($testCases as $testCase) {
-                    $testProviderArray[basename($testCase)] = [$testCase, $testCase . ".tree", $testCase . ".diag"];
-                }
+        foreach (self::PATTERNS_FOR_MINIMUM_PHP_VERSION as [$minVersionId, $filePattern]) {
+            if (PHP_VERSION_ID < $minVersionId) {
+                continue;
+            }
+
+            $testCases = glob($filePattern . ".php");
+            foreach ($testCases as $testCase) {
+                $testProviderArray[basename($testCase)] = [$testCase, $testCase . ".tree", $testCase . ".diag"];
             }
         }
 
